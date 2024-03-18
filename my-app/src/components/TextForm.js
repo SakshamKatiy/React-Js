7f5ea5165aa560d18a364a00e2d30d935c07461b
Saksham Katiyar <115263557+SakshamKatiy@users.noreply.github.com>
2024-03-18 11:55:07 +0530

    read words and characters
    add more logic

--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -12,18 +12,35 @@ const handleOnChange=(event)=>{
     console.log("On Change");
     setText(event.target.value);
 }
+const handleDownClick = () =>{
+    let DownText = text.toLowerCase();
+    setText(DownText);
+}
 
     const [text , setText] = useState('Enter text here');
     //text = "new state"  //wronge way to change text
     //setText("new text");  //correct way to change text
   return (
     <div>
-        <h1>{props.heading} - {text}</h1>
+        <div className="container">
+        <h1>{props.heading} </h1>
       <div className="form-group">
         <label ></label>
         <textarea className="form-control" name="" id="" value={text} rows="3" onChange={handleOnChange}></textarea>
       </div>
+
       <button type="submit" className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
+
+      <button type="submit" className="btn btn-danger mx-2" onClick={handleDownClick}>Convert to lowercase</button>
+      </div>
+
+      <div className="container">
+<h1>Your text summary</h1>
+<p>{text.split(" ").length} words and {text.length} characters</p>
+<p>{0.008 * text.split(" ").length} Minutes read</p>
+<h2>Preview</h2>
+<p>{text}</p>
+    </div>
     </div>
   )
 }
